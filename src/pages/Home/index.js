c1cf9374342b1655242feb57ea8b2eda2b93fdac
Felipe Borde <felipe.borde@inpartec.com.br>
2020-07-30 00:07:33 -0300

    Adicionando react-slick

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json'
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel';
+import Carrossel from '../../components/Carrossel';
 import Footer from '../../components/Footer';
 
 function Home() {
@@ -16,28 +16,28 @@ function Home() {
         videoDescription={"O que é Front-end?"}
       />
 
-      <Carousel
+      <Carrossel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
 
-      <Carousel
+      <Carrossel
         category={dadosIniciais.categorias[1]}
       />
 
-      <Carousel
+      <Carrossel
         category={dadosIniciais.categorias[2]}
       />
 
-      <Carousel
+      <Carrossel
         category={dadosIniciais.categorias[3]}
       />
 
-      <Carousel
+      <Carrossel
         category={dadosIniciais.categorias[4]}
       />
 
-      <Carousel
+      <Carrossel
         category={dadosIniciais.categorias[5]}
       />
 

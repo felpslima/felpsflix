8608d0b8a0d1b363456793e4a5604a4192887319
Felipe Borde <felipe.borde@inpartec.com.br>
2020-07-29 23:23:35 -0300

    Ajustes para transformar em SPA, usando react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+  BrowserRouter,
+  Switch, //Faz o "if" para validar as rotas
+  Route   //Declaração da rota
+} from 'react-router-dom';
+import Home from './pages/Home';
+import CadatroVideo from './pages/cadastro/Video';
+import CadatroCategoria from './pages/cadastro/Categoria';
+
+// CRIAR UMA PÁGINA 404
+const Pagina404 = () => (<div>404</div>); 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadatroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadatroCategoria} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
+  // SEM USAR REACT ROUTER
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
